77ae3c83c0752ccb8bcd254c51d772fe99d5a6e4
Amangulati05 <131166446+Amangulati05@users.noreply.github.com>
2025-07-16 20:07:03 +0530

    My commitment

--- aman.cpp
+++ aman.cpp
@@ -1,28 +1,23 @@
 #include<bits/stdc++.h>
+#include<conio.h>
 using namespace std;
-
-           
-            
-            void print(int n) { 
-                for(int i = 0; i <= 1; i++) { 
-                    for(int j = 0; j < 2*n - (2*i + 1); j++) { 
-                        cout << "*"; 
-                    } 
-                    for(int j = 0; j < i; j++) { 
-                        cout << " "; 
-                    } 
-                    cout << endl; 
-                } 
-            }  
-            
-            int main() { 
-                int t; 
-                cin>>t; 
-                for(int i = 0; i < t; i++) { // Fixed loop syntax
-                    int n; 
-                    cin>>n; 
-                    print(n); 
-                } 
-                return 0; // Added return statement for correct execution
-            }
+ int cnt =0;
+ void print(int n){
+    if(cnt>n)return ;
+        cout<<cnt<<endl;
+        cnt++;
+     
+     
+        
     
+       print(n);
+    
+    
+ }
+ int main(){
+    int n;
+    cin>>n;
+
+    print(n);
+    return 0;
+ }
